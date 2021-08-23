601fa10e55724f5e5686cd6304d071b8a5811b64
Sidharth Bansal <59787009+MARTIANSID@users.noreply.github.com>
2021-08-24 01:00:39 +0530

    clear code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React,{useState} from "react";
-import "./App.css";
-import Login from "./components/Login";
-import useLocalStorage from './hooks/useLocalStorage'
-import Dashboard from './components/Dashboard'
 
 
 
 function App() {
-  const [id, setId] = useLocalStorage("id");
-  const handleId = (id) => {
-    setId(id);
-  };
+ 
   return (
     <div> <Dashboard id={id}/> </div>
   )
